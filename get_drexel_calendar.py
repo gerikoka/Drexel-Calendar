7d63571f127e5e5ad1d944758c08f29c1a8e00aa
Rachelle Pena <rmp356@drexel.edu>
2023-11-07 12:56:18 -0500

    put code into a function to be used in other files

--- get_drexel_calendar.py
+++ get_drexel_calendar.py
@@ -2,34 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 
-url = 'https://drexel.edu/provost/policies-calendars/academic-calendars/quarters/'
-r = requests.get(url) 
-  
-soup = BeautifulSoup(r.content, 'html5lib')
-table = soup.find('table', class_='DUVendors').find('tbody')
-events = []
+def scrape_events():
+    url = 'https://drexel.edu/provost/policies-calendars/academic-calendars/quarters/'
+    r = requests.get(url) 
+    soup = BeautifulSoup(r.content, 'html5lib')
+    table = soup.find('table', class_='DUVendors').find('tbody')
+    events = []
 
-for row in table.findAll('tr'):
-    event = {}
-    rowInfo = []
-    
-    for column in row.findAll('td'):
-        non_strikethrough_text = ''.join([str(item) for item in column.contents if isinstance(item, str)])
-        rowInfo.append(non_strikethrough_text.strip())
+    for row in table.findAll('tr'):
+        event = {}
+        rowInfo = []
+        
+        for column in row.findAll('td'):
+            non_strikethrough_text = ''.join([str(item) for item in column.contents if isinstance(item, str)])
+            rowInfo.append(non_strikethrough_text.strip())
 
-    if 'TBD' in rowInfo[1]:  # Skip rows with 'TBD' dates
-        continue
+        if 'TBD' in rowInfo[1]:  # Skip rows with 'TBD' dates
+            continue
 
-    event_date = rowInfo[1]
-    event_title = rowInfo[2]
+        event_date = rowInfo[1]
+        event_title = rowInfo[2]
 
-    try: 
-        event['date'] = datetime.datetime.strptime(event_date, '%A, %B %d, %Y').strftime('%Y-%m-%d')
-    except ValueError:
-        print(f"Error parsing date: {event_date}")
-        continue  # Skip rows with invalid date formats
+        try: 
+            event['date'] = datetime.datetime.strptime(event_date, '%A, %B %d, %Y').strftime('%Y-%m-%d')
+        except ValueError:
+            print(f"Error parsing date: {event_date}")
+            continue  # Skip rows with invalid date formats
 
-    event['title'] = event_title
-    events.append(event)
+        event['title'] = event_title
+        events.append(event)
 
-print(events[0])
+    return events
+
+if __name__ == "__main__":
+    events = scrape_events()
+    print(events)
