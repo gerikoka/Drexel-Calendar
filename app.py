b81fac368abbb6e2b2af69ba025509a92bee3104
Rachelle Pena <rmp356@drexel.edu>
2024-01-31 16:37:30 -0500

    add login feature

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ from bs4 import BeautifulSoup
 import datetime
 import os
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, redirect, url_for, render_template, send_from_directory
+from flask import request
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_login import UserMixin, login_user
+from flask_login import current_user
+
 
 def scrape_events():
     url = 'https://drexel.edu/provost/policies-calendars/academic-calendars/quarters/'
@@ -38,26 +45,101 @@ def scrape_events():
     return events
 
 app = Flask(__name__, static_folder='static')
+ 
+# tells flask-sqlalchemy what database to connect to
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+# enter a secret key
+app.config["SECRET_KEY"] = "ENTER YOUR SECRET KEY"
+# initialize flask-sqlalchemy extension
+db = SQLAlchemy()
+ 
+# LoginManager is needed for our application 
+# to be able to log in and out users
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-@app.route('/')
-def index():
-    events = scrape_events()
-    return render_template('index.html', events=events)
+
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+ 
+ 
+db.init_app(app)
+ 
+ 
+with app.app_context():
+    db.create_all()
+ 
+ 
+@login_manager.user_loader
+def loader_user(user_id):
+    return Users.query.get(user_id)
+ 
+ 
+@app.route('/sign_up.html', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        user = Users(username=request.form.get("username"),
+                     password=request.form.get("password"))
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("login"))
+    return render_template("sign_up.html")
+ 
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    if request.method == 'POST':
+        # Process the form data here
+        user = Users(username=request.form.get("username"),
+                     password=request.form.get("password"))
+        db.session.add(user)
+        db.session.commit()
+
+        # Perform the necessary actions to handle the sign-up (e.g., store user in the database)
+
+        # Redirect to a success page or login page
+        return redirect(url_for('login'))
+ 
+@app.route("/login.html", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = Users.query.filter_by(
+            username=request.form.get("username")).first()
+        print("username:", request.form.get("username"))
+        if user.password == request.form.get("password"):
+            login_user(user)
+            return redirect(url_for("home"))
+    return render_template("login.html")
+ 
+ 
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/index.html')
-def homepage():
+@app.route('/')
+def home():
     events = scrape_events()
-    return render_template('index.html', events=events)
+    if current_user.is_authenticated:
+        username = current_user.username
+        return render_template('index.html', events=events, username=username)
+    else: 
+        return render_template('index.html', events=events)
 
-@app.route('/tester.html')
+@app.route('/todo.html')
+def todo():
+    return render_template('todo.html')
+
+@app.route('/tester')
 def tester():
     events = scrape_events()
     return render_template('tester.html', events=events)
-
 
 if __name__ == "__main__":
     app.run()
